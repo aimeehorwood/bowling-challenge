20f4a9214f350382a3ee44a0e84104b83c01c1bd
Aimee <aimeehorwood@hotmail.com>
2022-12-30 15:00:31 +0000

    adds strikes spares to frame class

--- frame.js
+++ frame.js
@@ -2,14 +2,26 @@ class Frame {
   constructor() {
     this.rolls = [];
   }
-  
-  
-  
-  
-    addRoll(pins) {
-        this.rolls.push(pins)
-    }
 
+  addRoll(pins) {
+    this.rolls.push(pins);
+  }
+
+  score() {
+    let sum = 0;
+    for (let i = 0; i < this.rolls.length; i++) {
+      sum += this.rolls[i];
+    }
+    return sum;
+  }
+
+  isStrike() {
+    return this.rolls[0] === 10;
+  }
+
+  isSpare() {
+    return this.rolls[0] + this.rolls[1] === 10;
+  }
 }
 
 module.exports = Frame;
--- frame.test.js
+++ frame.test.js
@@ -8,10 +8,57 @@ describe("Frame", () => {
     });
 
     it("can receive two rolls", () => {
-        const frame = new Frame();
-        frame.addRoll(5);
-        frame.addRoll(4);
-        expect(frame.rolls.length).toEqual(2);
+      const frame = new Frame();
+      frame.addRoll(5);
+      frame.addRoll(4);
+      expect(frame.rolls.length).toEqual(2);
     });
   });
+
+  describe("score", () => {
+    it("can return the score from a frame with two rolls", () => {
+      const frame = new Frame();
+      frame.addRoll(5);
+      frame.addRoll(4);
+      expect(frame.score()).toEqual(9);
+    });
+  });
+
+  describe("isStrike", () => {
+    it("returns false if no strike", () => {
+      const frame = new Frame();
+      frame.addRoll(5);
+      frame.addRoll(4);
+      expect(frame.isStrike()).toEqual(false);
+    });
+
+    it("returns true if  strike", () => {
+      const frame = new Frame();
+      frame.addRoll(10);
+      expect(frame.isStrike()).toEqual(true);
+    });
+  });
+  describe("isSpare", () => {
+    it("returns false if no spare", () => {
+      const frame = new Frame();
+      frame.addRoll(5);
+      frame.addRoll(4);
+      expect(frame.isSpare()).toEqual(false);
+    });
+    
+    it("returns true if spare", () => {
+      const frame = new Frame();
+      frame.addRoll(5);
+      frame.addRoll(5);
+      expect(frame.isSpare()).toEqual(true);
+    });
+
+    it("returns false when a strike has been scored", () => {
+            const frame = new Frame();
+            frame.addRoll(10);
+            expect(frame.isSpare()).toEqual(false);
+          
+
+        });
+    })
 });
